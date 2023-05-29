ff24bc1e6bf3465585b5be612a5319d711721c6f
Regina Timirgaleeva <sadikovaregina@mail.ru>
2023-05-29 21:15:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+
+        int ticketPrice = 500;
+        int rubMili = ticketPrice / 20;
+        System.out.println("Бонусы: " + rubMili);
+
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
